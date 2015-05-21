efaccdc5f173901ef3a0f28e5d3672ab5ce76f1d
davy <davy@qipp.com>
2015-05-21 09:45:52 +0200

    First commit.

---
+++ qipp-services-utils.js
@@ -0,0 +1,39 @@
+/* global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('qipp-services-utils', [])
+
+        .factory('$analytics', ['$window', function ($window) {
+            return function (method) {
+                var analytics = $window.analytics;
+                return analytics[method].apply(
+                    analytics,
+                    [].slice.call(arguments, 1)
+                );
+            };
+        }])
+
+        .factory('$cssProperties', function () {
+            return function (element) {
+                var properties = {};
+                angular.forEach(['webkitTrans', 'MozTrans', 'trans'], function (prop) {
+                    if (element[0].style[prop + 'itionDuration'] !== undefined) {
+                        properties.transitionDuration = prop + 'itionDuration';
+                        properties.transitionEnd = prop === 'webkitTrans' ?
+                            'webkitTransitionEnd' : 'transitionend';
+                        if (element[0].style[prop + 'form'] !== undefined) {
+                            properties.transform = prop + 'form';
+                        }
+                    }
+                });
+                return properties;
+            };
+        })
+
+        .factory('$windowLocation', ['$window', function ($window) {
+            return $window.location;
+        }]);
+
+}());
